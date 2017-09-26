8e40d5cd6a947fa33a29f2568e55e6558d4917d0
Yong Feng <286606198@qq.com>
2017-09-26 13:38:13 +0800

    Update device.py

--- TensorAPI/device.py
+++ TensorAPI/device.py
@@ -2,10 +2,19 @@
 import tensorflow as tf
 
 # Creates a graph.
-a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
-b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+with tf.device('/cpu:0'):
+  a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+  b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
 c = tf.matmul(a, b)
+
+'''
+config = tf.ConfigProto()
+config=tf.ConfigProto(log_device_placement=True)
+config.gpu_options.allow_growth=True 
+sess = tf.Session(config=config)
+'''
+
 # Creates a session with log_device_placement set to True.
-sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=True))
 # Runs the op.
 print(sess.run(c))
