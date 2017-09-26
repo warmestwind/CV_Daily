b17dfb7696b4fe854ec833ac088c958433a5a117
Yong Feng <286606198@qq.com>
2017-09-26 12:46:13 +0800

    Update act.py

--- Complete workflow of Traffic sign/act.py
+++ Complete workflow of Traffic sign/act.py
@@ -1,19 +1,23 @@
 #import image_preprocess #第二行足够，但是只写import image_preprocess 不行
-from image_preprocess import  load_data, image_scaling, label_convert
+from image_preprocess import  load_data, image_scaling
 import os
 import matplotlib.pyplot as plt 
-from train3 import graph, define_variable
+#from train_summary import graph, define_variable
+#from train_restore import graph, define_variable
+#from test import graph, define_variable
+from infer import graph, define_variable
 import tensorflow as tf
 import numpy as np
 
 def action():
     #define dir
-    ROOT_PATH = "F:\TensorFlow"
+    ROOT_PATH = "F:\SourceCode\Python\Traffic_sign"
     train_data_directory = os.path.join(ROOT_PATH, "TrafficSigns\Training")
     test_data_directory = os.path.join(ROOT_PATH, "TrafficSigns\Testing")
 
     #load data
-    images, labels = load_data(train_data_directory)
+    #images, labels = load_data(train_data_directory)
+    images, labels = load_data(test_data_directory)
     '''
     im=images[14]
     plt.imshow(im)
@@ -21,10 +25,6 @@ def action():
     '''
     #images preprocess
     images28=image_scaling(images)
-    
-    #labels to one_hot
-    #labels=label_convert(labels)
-    #print(type(labels))
 
     #train lenet
     graph(images28,labels)
