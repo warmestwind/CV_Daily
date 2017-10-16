dc013a303d4495529af258379930138390ae13a0
Yong Feng <286606198@qq.com>
2017-10-16 15:47:19 +0800

    Update image_preproc.py

--- Python_basic/image_preproc.py
+++ Python_basic/image_preproc.py
@@ -7,7 +7,7 @@ img[1,1]=[0,0,0]
 cv2.imwrite('red11.jpg',img)
 print(img[0,0,2])
 #[x,y,z]  x: row  y: column z:index of color component
-img_rgg = img[:, :, ::-1]
+img_rgb = img[:, :, ::-1]
 # ::-1 ： reverse the component
 print(img_rgb[:,:])
 #cv2.waitKey(0)
