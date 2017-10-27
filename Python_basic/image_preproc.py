d7f01dacb2d189e6c5eb5f94d5dfb1a9c35014ac
Yong Feng <286606198@qq.com>
2017-10-27 16:53:04 +0800

    Update image_preproc.py

--- Python_basic/image_preproc.py
+++ Python_basic/image_preproc.py
@@ -1,15 +1,21 @@
 import cv2
-
+import numpy as np
 img=cv2.imread('red10.jpg',1)
-#img2=img.copy()
+img2=img.copy()
 img[0,0]=[255,0,0]
 img[1,1]=[0,0,0]
+img2[0,0]=[0,0,0]
+img2[0,1]=[0,0,0]
+img2[1,0]=[0,0,0]
+img2[1,1]=[0,0,0]
 #cv2.imshow('image',img)
-cv2.imwrite('red11.jpg',img)
-print(img[0,0,2])
-#[x,y,z]  x: row  y: column z:index of color component
+#cv2.imwrite('red11.jpg',img)
+#print(img[0,0,2])
 img_rgb = img[:, :, ::-1]
-# ::-1 ： reverse the component
-print(img_rgb[:,:])
-#print(img_rgb.shape)
+print(img_rgb.shape) # (2,2,3）
+print(img_rgb[0,0]) # (2,2,3）
+#print(np.mean(img[:,:,0]))
+#print((np.mean(img2[:,:,0])+np.mean(img[:,:,0]))/2)
+
+
 #cv2.waitKey(0)
