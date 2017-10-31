6e56e549a85cd3527526e6d7e72f10502ee2b959
Yong Feng <286606198@qq.com>
2017-10-31 17:06:43 +0800

    Create os_exists.py

---
+++ Python_basic/os_exists.py
@@ -0,0 +1,8 @@
+import os
+annotation_file='Data_zoo/MIT_SceneParsing/ADEChallengeData2016\\a\
+nnotations\\training\\ADE_train_00000004.png'
+annotation_file2='F:\\SourceCode\\GitHub\\FCN.tensorflow-master\\Data_zoo\\MIT_SceneParsing\\ADEChallengeData2016\\a\
+nnotations\\training\\ADE_train_00000004.png'
+print(annotation_file)
+print(annotation_file2)
+print(os.path.exists(annotation_file))
