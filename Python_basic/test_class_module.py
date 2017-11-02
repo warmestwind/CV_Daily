b8132542e663e2a1aed4a60bd28afc589a4a2e53
Yong Feng <286606198@qq.com>
2017-11-02 13:17:43 +0800

    Update test_class_module.py

--- Python_basic/test_class_module.py
+++ Python_basic/test_class_module.py
@@ -1,7 +1,14 @@
 import class_module
-
-a=class_module.a1(1)
+import sys
+import os
+a=sys_modules.a1(1)
 print(a.c)
 
 print('module=',class_module.a1.__module__)
 print('module=',a.__module__)
+
+print(sys.modules[a.__module__].__file__)
+path1=os.path.pardir
+print(path1)
+path2=sys.modules[a.__module__].__file__
+print(os.path.abspath(os.path.join(path2,path1)))
