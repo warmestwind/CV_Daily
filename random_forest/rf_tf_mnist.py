e434b05d0ed2071f4cb9040989a977a5f05c6338
Yong Feng <286606198@qq.com>
2017-12-18 09:19:36 +0800

    Create rf_tf_mnist.py

---
+++ random_forest/rf_tf_mnist.py
@@ -0,0 +1,66 @@
+//reference: https://github.com/tensorflow/tensorflow/tree/r1.4/tensorflow/contrib/tensor_forest
+import tensorflow as tf
+from tensorflow.contrib.tensor_forest.python import tensor_forest
+
+# standard method for import MNIST data 
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("./data", one_hot=False)
+
+# Parameters
+num_steps = 500 # Total steps to train
+batch_size = 1024 # The number of samples per batch
+num_classes = 10 # The 10 digits
+num_features = 784 # Each image is 28x28 pixels, every pixel is a feature
+num_trees = 10
+max_nodes = 1000
+
+# Random Forest Parameters
+# fill():intelligently sets any non-specific parameters
+hparams = tensor_forest.ForestHParams(num_classes=num_classes,
+                                      num_features=num_features,
+                                      regression=False,
+                                      num_trees=num_trees,
+                                      max_nodes=max_nodes).fill()
+
+
+# Input and Target data
+X = tf.placeholder(tf.float32, shape=[None, num_features])
+# For random forest, labels must be integers (the class id)
+#shape(Y)=[None] because not it's one_hot label
+Y = tf.placeholder(tf.int32, shape=[None])   
+
+
+# Build the Random Forest
+forest_graph = tensor_forest.RandomForestGraphs(hparams)
+# Get training graph and loss
+train_op = forest_graph.training_graph(X, Y)
+loss_op = forest_graph.training_loss(X, Y)
+
+# Measure the accuracy
+infer_op = forest_graph.inference_graph(X)
+correct_prediction = tf.equal(tf.argmax(infer_op, 1), tf.cast(Y, tf.int64))
+#because X is totol instances ,accuracy is average of all instance
+accuracy_op = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+# Initialize the variables (i.e. assign their default value)
+init_vars = tf.global_variables_initializer()
+
+# Start TensorFlow session
+sess = tf.Session()
+
+# Run the initializer
+sess.run(init_vars)
+
+# Training
+for i in range(1, num_steps + 1): #[1,501)
+    # Prepare Data
+    # Get the next batch of MNIST data (only images are needed, not labels)
+    batch_x, batch_y = mnist.train.next_batch(batch_size)
+    _, l = sess.run([train_op, loss_op], feed_dict={X: batch_x, Y: batch_y})
+    if i % 50 == 0 or i == 1:
+        acc = sess.run(accuracy_op, feed_dict={X: batch_x, Y: batch_y})
+        print('Step %i, Loss: %f, Acc: %f' % (i, l, acc))
+
+# Test Model
+test_x, test_y = mnist.test.images, mnist.test.labels
+print("Test Accuracy:", sess.run(accuracy_op, feed_dict={X: test_x, Y: test_y}))
