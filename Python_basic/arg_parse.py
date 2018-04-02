e2dcfd7ba0f440ecab5d6cc21c8abbe7d726ad2c
Yong Feng <286606198@qq.com>
2018-04-02 16:46:27 +0800

    Update arg_parse.py

--- Python_basic/arg_parse.py
+++ Python_basic/arg_parse.py
@@ -5,8 +5,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument("square", help="display a square of a given number",
                     type=int)
 #optional arguments are unnecessary to transmit, dest ,default only in optional argu                 
-parser.add_argument("--add", dest='bias',default=2, help="add a int to square", type=int)
+parser.add_argument("--add", dest='bias',default=2, help="add a int to square", type=int
 
+#if spycify minibatch then minibatch is True                              
+parser.add_argument('--minibatch', action='store_true',
+                     help='use minibatch discrimination')
 # Parsing arguments
 args = parser.parse_args()
 
@@ -14,3 +17,6 @@ args = parser.parse_args()
 print(args.square**2+args.bias)
 #else :print(args.square**2) # py arg_parse.py 2 --add=1/--add 1
 print(args.bias)
+
+# usage:
+# python arg_parse.py --minibatch                     
