cc6bdb169c8c8cb023416bcc09f04a9ec83c83e3
Yong Feng <286606198@qq.com>
2018-12-18 13:51:08 +0800

    Create try_exception_else_finally.py

---
+++ Python_basic/try_exception_else_finally.py
@@ -0,0 +1,14 @@
+# https://realpython.com/python-exceptions/
+
+for i in range(5):
+    try:
+        if i == 3:
+            raise Exception('x should not be 3. The value of x was: {}'.format(i))
+        else:
+            print(i)
+    except Exception as exc:
+         #print exc
+         #continue
+        break
+        #pass
+        
